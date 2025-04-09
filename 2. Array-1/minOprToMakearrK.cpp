75e259fdf13733de55ed475d4c2e7276d5e713ae
Amit <amitgupta561710@gmail.com>
2025-04-09 14:37:35 +0530

    09-04-2025

---
+++ 2. Array-1/minOprToMakearrK.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSame(vector<int> &nums, int k){
+        int maxVal = *max_element(nums.begin(), nums.end());
+        return maxVal == k;
+    }
+    int minOperations(vector<int>& nums, int k) {
+        int minVal = *min_element(nums.begin(), nums.end());
+        int operation = 0;
+        if(minVal < k) return -1;
+        while(true){
+            if(isSame(nums, k)) return operation;
+            int fMax = -1, sMax = -1;
+            for(auto it : nums){
+                if (it > fMax) {
+                    sMax = fMax; fMax = it;
+                } else if (it > sMax && it != fMax) {
+                    sMax = it;
+                }
+            }
+            if(sMax == -1) sMax = k;
+            for(int i = 0; i < nums.size(); i++){
+                if(nums[i] == fMax) nums[i] = sMax;
+            }
+            operation++;
+        }
+        return 0;
+    }
+};
+
+int main(){
+
+}
